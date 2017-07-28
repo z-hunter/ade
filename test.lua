2be2faf40ef80d1adb1c9d576455b2ff746d5295
z-hunter <z-hunter@tut.by>
2017-07-28 18:44:12 +0300

    debug

--- test.lua
+++ test.lua
@@ -2,7 +2,7 @@ require "findfuzzy"
 local fuzzel = require("fuzzel")
 local utf8 = require 'lua-utf8'
 
-s= " Мерседес Е-бург W210 (02-09) крыло заднее левое"
+s= "Хендай Н100 Портер 1 панель кузова правая"
 dofile "parts.lua"
 dofile "models.lua"
 
@@ -16,6 +16,28 @@ function dumpArray(T)
 	  print (k,v)
    end
 end
+
+
+function defakeLatinLetters(s)
+   local T={
+	  ["К"]="K",
+	  ["Е"]="E",
+	  ["Н"]="H",
+	  ["Х"]="X",
+	  ["В"]="B",
+	  ["А"]="A",
+	  ["Р"]="P",
+	  ["О"]="O",
+	  ["С"]="C",
+	  ["М"]="M",
+	  ["Т"]="T",
+   }
+   for k,v in pairs(T) do
+	  utf8.gsub(s,k,v)
+   end
+   return s
+end
+
 
 function detectMark(s)
    local P={}
@@ -39,11 +61,14 @@ end
 
 function detectModel(s, mark)
    local P={}
+
    for f,v in pairs(Models[mark]) do
 	  if f ~="nam" then		 
 		 table.insert(P, f)
 		 if  v ~="" then
 			table.insert(P, v)
+		 else
+			table.insert(P, defakeLatinLetters(v))
 		 end    
 	  end
    end
@@ -62,11 +87,33 @@ function detectModel(s, mark)
   end
 end
 
+function detectPart(s)
+   local P={}
+   for f,v in pairs(Parts) do
+	  table.insert(P, f)
+      if  v ~="" then table.insert(P, v) end    
+   end
+
+   local r = recognizeFuzzyPatterns(s, P)
+   
+   if not r then
+	  return ("Прочая запчасть")
+   elseif not Parts[r] then
+	  for f,v in pairs(Parts) do
+		 if v == r then
+			return f
+		 end
+	  end
+   else
+	  return r
+   end
+end
 
 mk=detectMark(s)
 print "-----"
 md=detectModel(s,mk)
-print (s,"=",mk, md)
+pt=detectPart(s)
+print (s,"=",mk, md, pt)
 
 
   
